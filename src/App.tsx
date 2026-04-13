bafbda2a83a9c25ac7b59bba0d587740ceaef344
jrs-neto <jrd.santosneto@gmail.com>
2026-04-13 10:46:28 -0300

    ✨ New Feature: Configuração de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import Footer from "./assets/components/footer/Footer";
-import Home from "./assets/components/home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./assets/components/navbar/Navbar";
+import Home from "./assets/components/home/Home";
+import Footer from "./assets/components/footer/Footer";
+
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <div className="flex-1">
-        <Home />
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/assets/components/navbar/Navbar.tsx
+++ src/assets/components/navbar/Navbar.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <div className="w-full bg-blue-600 text-white flex justify-center py-4">
       <div className="container flex justify-between items-center text-lg">
-        <h1 className="text-2xl font-bold">Farmácia</h1>
+        <Link to="/home" className="text-2xl font-bold uppercase">Farmácia</Link>
 
         <div className="flex gap-4">
-          <span>Home</span>
+          <Link to="/home">Home</Link>
           <span>Categorias</span>
         </div>
       </div>
